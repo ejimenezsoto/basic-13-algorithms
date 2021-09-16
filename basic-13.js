e9876597954a4d92b809c78d307bc7bb8e251684
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-09-16 09:21:07 -0700

    Add basic 13 functions

---
+++ basic-13.js
@@ -0,0 +1,101 @@
+//1
+// print numbers 1-255
+function Print1To255() {
+
+}
+
+//2
+// print only odd numbers 1-255
+function PrintOdds1To255() {
+
+}
+
+//3
+// print the sum + sum of 0 - 255
+// e.g. 0 , 1 , 3, 4, 7, 11...
+function PrintIntsAndSum0To255() {
+
+}
+
+
+//4
+// print values of array
+var arr = [-3,2,-1,0,1,-4,6,7,10]
+
+function PrintArrayVals(arr) {
+
+}
+
+//5
+// print max of array
+function PrintMaxOfArray(arr) {
+
+}
+
+//6
+// print array average
+function PrintAverageOfArray(arr) {
+
+}
+
+//7
+// return new array 1-255 only odds
+function ReturnOddsArray1To255() {
+
+}
+
+//8
+// square array values
+function SquareArrayValues(arr) {
+
+}
+
+//9
+// return count of values in array greater than y
+function ReturnArrayCountGreaterThanY(arr, y) {
+
+}
+//10
+// replace negative values in array with 0
+function ZeroOutArrayNegativeVals(arr) {
+
+}
+//11
+// print max min and average values of array
+function PrintMaxMinAverageArrayVals(arr) {
+
+}
+//12
+// shift values of array left
+function ShiftArrayValsLeft(arr) {
+
+}
+//13
+// swap string for negative values in array
+function SwapStringForArrayNegativeVals(arr) {
+
+}
+
+
+/*
+
+
+console.log(ShiftArrayValsLeft([-1, 3, -5, 2, 4]))
+
+console.log(PrintMaxMinAverageArrayVals([-1, 3, -5, 2, 4]))
+
+console.log(PrintMaxOfArray([1, 2, 2, 2, 2, 10]));
+
+console.log(SwapStringForArrayNegativeVals([-1, 3, -5, 2, 4]))
+
+console.log(ReturnArrayCountGreaterThanY([1, 3, 5, 2, 4], 2))
+
+console.log(SquareArrayValues([1, 3, 5, 2, 4]))
+
+console.log(PrintAverageOfArray([2, 2, 2, 2, 10]));
+
+console.log(PrintIntsAndSum0To255())
+
+console.log(Print1To255())
+
+*/
