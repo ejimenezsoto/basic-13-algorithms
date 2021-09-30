d526fe5f0390045f75bcde35e9fed47b6502746e
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-09-29 17:13:52 -0700

    Clean Code and Comments

--- basic-13.js
+++ basic-13.js
@@ -1,23 +1,23 @@
 //1
 // print numbers 1-255
 function Print1To255() {
-  for(var i=1;i<=255;i++){
-    console.log(i);
-  }
+  
 }
-Print1To255();
+
+//Print1To255();
 
 //2
 // print only odd numbers 1-255
 function PrintOdds1To255() {
-
+  
 }
+
 
 //3
 // print the sum + sum of 0 - 255
 // e.g. 0 , 1 , 3, 4, 7, 11...
 function PrintIntsAndSum0To255() {
-
+  
 }
 
 
@@ -32,12 +32,14 @@ function PrintArrayVals(arr) {
 function PrintMaxOfArray(arr) {
 
 }
+// console.log(PrintMaxOfArray([1, 2, 2, 2, 2, 10]));
 
 //6
 // print array average
 function PrintAverageOfArray(arr) {
 
 }
+// console.log(PrintAverageOfArray([2, 2, 2, 2, 10]));
 
 //7
 // return new array 1-255 only odds
@@ -50,53 +52,49 @@ function ReturnOddsArray1To255() {
 function SquareArrayValues(arr) {
 
 }
+// console.log(SquareArrayValues([1, 3, 5, 2, 4]))
+
 
 //9
 // return count of values in array greater than y
 function ReturnArrayCountGreaterThanY(arr, y) {
 
 }
+// console.log(ReturnArrayCountGreaterThanY([1, 3, 5, 2, 4], 2))
+
+
 //10
 // replace negative values in array with 0
 function ZeroOutArrayNegativeVals(arr) {
 
 }
+//console.log(ZeroOutArrayNegativeVals([2,32,0,-4,7,-9,-1,12,-6]))
+
 //11
 // print max min and average values of array
 function PrintMaxMinAverageArrayVals(arr) {
 
 }
+// console.log(PrintMaxMinAverageArrayVals([-1, 3, -5, 2, 4]))
+
+
+
 //12
 // shift values of array left
 function ShiftArrayValsLeft(arr) {
 
 }
+//console.log(ShiftArrayValsLeft([-1, 3, -5, 2, 4]))
+
+
+
+
 //13
 // swap string for negative values in array
 function SwapStringForArrayNegativeVals(arr) {
 
 }
+// console.log(SwapStringForArrayNegativeVals([-1, 3, -5, 2, 4]))
 
 
-/*
 
-
-console.log(ShiftArrayValsLeft([-1, 3, -5, 2, 4]))
-
-console.log(PrintMaxMinAverageArrayVals([-1, 3, -5, 2, 4]))
-
-console.log(PrintMaxOfArray([1, 2, 2, 2, 2, 10]));
-
-console.log(SwapStringForArrayNegativeVals([-1, 3, -5, 2, 4]))
-
-console.log(ReturnArrayCountGreaterThanY([1, 3, 5, 2, 4], 2))
-
-console.log(SquareArrayValues([1, 3, 5, 2, 4]))
-
-console.log(PrintAverageOfArray([2, 2, 2, 2, 10]));
-
-console.log(PrintIntsAndSum0To255())
-
-console.log(Print1To255())
-
-*/
